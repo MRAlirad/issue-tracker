b2e5a741f460bf4cca1efc38c2bb6dd781da292e
MRAlirad <m.r.alirad75@gmail.com>
2023-10-25 21:57:47 +0330

    page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,12 +2,10 @@ import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import Link from '../components/Link';
 import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
 import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
 	const issues = await prisma.issue.findMany();
-	await delay(4000);
 	return (
 		<div>
 			<IssueActions />
