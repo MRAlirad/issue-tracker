73a0c1a4ec0909da26472084d20ab3ab512c7e20
MRAlirad <m.r.alirad75@gmail.com>
2023-10-25 21:26:44 +0330

    additional lazy loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Flex, Card, Box } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
-const LoadingIssueDetailPage = () => {
-  return (
-    <div>Loading...</div>
-  )
-}
+const LoadingIssueDetailPage = async () => {
+	return (
+		<Box className="max-w-xl">
+			<Skeleton />
+			<Flex
+				className="space-x-3"
+				my="2"
+			>
+				<Skeleton width="5rem" />
+				<Skeleton width="8rem" />
+			</Flex>
+			<Card
+				className="prose"
+				mt="4"
+			>
+				<Skeleton count={3} />
+			</Card>
+		</Box>
+	);
+};
 
-export default LoadingIssueDetailPage
+export default LoadingIssueDetailPage;
