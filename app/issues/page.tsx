3e392e0c8242301e603940ec509bb634c1397fd9
MRAlirad <m.r.alirad75@gmail.com>
2023-10-25 17:36:27 +0330

    showing issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -7,30 +7,32 @@ import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
 	const issues = await prisma.issue.findMany();
-	await delay(4000)
+	await delay(4000);
 	return (
 		<div>
-			<IssueActions/>
+			<IssueActions />
 
 			<Table.Root variant="surface">
 				<Table.Header>
 					<Table.Row>
 						<Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className='hidden sm:table-cell'>Status</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className='hidden sm:table-cell'>Created</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className="hidden sm:table-cell">Status</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className="hidden sm:table-cell">Created</Table.ColumnHeaderCell>
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
 					{issues.map((issue) => (
 						<Table.Row key={issue.id}>
 							<Table.Cell>
-								{issue.title}
-								<div className='block sm:hidden'>
+								<Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+								<div className="block sm:hidden">
 									<IssueStatusBadge status={issue.status} />
 								</div>
 							</Table.Cell>
-							<Table.Cell className='hidden sm:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-							<Table.Cell className='hidden sm:table-cell'>{issue.createAt.toDateString()}</Table.Cell>
+							<Table.Cell className="hidden sm:table-cell">
+								<IssueStatusBadge status={issue.status} />
+							</Table.Cell>
+							<Table.Cell className="hidden sm:table-cell">{issue.createAt.toDateString()}</Table.Cell>
 						</Table.Row>
 					))}
 				</Table.Body>
