759d57404a342e11df10a212d8995e025da4e65d
MRAlirad <m.r.alirad75@gmail.com>
2023-10-25 14:56:45 +0330

    extract the rerror message compoent

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -51,14 +52,7 @@ const NewIssuePage = () => {
 						{...register('title')}
 					/>
 				</TextField.Root>
-				{errors.title && (
-					<Text
-						color="red"
-						as="p"
-					>
-						{errors.title.message}
-					</Text>
-				)}
+				<ErrorMessage>{errors.title?.message}</ErrorMessage>
 				<Controller
 					name="description"
 					control={control}
@@ -69,14 +63,7 @@ const NewIssuePage = () => {
 						/>
 					)}
 				/>
-				{errors.description && (
-					<Text
-						color="red"
-						as="p"
-					>
-						{errors.description.message}
-					</Text>
-				)}
+				<ErrorMessage>{errors.description?.message}</ErrorMessage>
 				<Button>Submit New Issue</Button>
 			</form>
 		</div>
