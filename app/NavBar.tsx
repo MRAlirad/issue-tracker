6bec8f20fa6918d6be7f91ef80049a027faf72e6
MRAlirad <m.r.alirad75@gmail.com>
2023-10-26 21:10:10 +0330

    user dropdown

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
 	const currentPath = usePathname();
@@ -14,7 +14,6 @@ const NavBar = () => {
 		{ label: 'Dashboard', href: '/' },
 		{ label: 'Issues', href: '/issues/list' },
 	];
-
 	return (
 		<nav className="border-b mb-5 px-5 py-3">
 			<Container>
@@ -44,7 +43,28 @@ const NavBar = () => {
 						</ul>
 					</Flex>
 					<Box>
-						{status === 'authenticated' && <Link href="/api/auth/signout">Log out</Link>}
+						{status === 'authenticated' && (
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									<Avatar
+										src={session?.user?.image!}
+										fallback="?"
+										size="2"
+										radius="full"
+										className="cursor-pointer"
+									/>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content>
+									<DropdownMenu.Label>
+
+										<Text size="2">{session?.user?.email}</Text>
+									</DropdownMenu.Label>
+									<DropdownMenu.Item>
+										<Link href="/api/auth/signout">Log out</Link>
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
+						)}
 						{status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
 					</Box>
 				</Flex>
