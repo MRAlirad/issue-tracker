3b8cc91e7fdb84bca7dd96d389bc6e0396e7fab1
MRAlirad <m.r.alirad75@gmail.com>
2023-10-28 22:10:39 +0330

    optimize performance qeury cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,16 +7,17 @@ import IssueDetails from './IssueDetails';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
+import { cache } from 'react';
 
 interface Props {
 	params: { id: string };
 }
 
+const fetchUser = cache((issueId: number)=> prisma.issue.findUnique({where: { id: issueId }}))
+
 const IssueDetailPage = async ({ params }: Props) => {
 	const session = await getServerSession(authOptions);
-	const issue = await prisma.issue.findUnique({
-		where: { id: parseInt(params.id) },
-	});
+	const issue = await fetchUser(parseInt(params.id));
 
 	if (!issue) notFound();
 
@@ -45,7 +46,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-	const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } });
+	const issue = await fetchUser(parseInt(params.id));
 
 	return {
 		title: issue?.title,
