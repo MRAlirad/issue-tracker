d0d02bcf9d4c61d66621e7234ea3ce9355cc3efe
MRAlirad <m.r.alirad75@gmail.com>
2023-10-28 14:51:21 +0330

    implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,16 +1,27 @@
+'use client'
+
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
 
 interface Props {
 	itemCount: number;
 	pageSize: number;
 	currentPage: number;
 }
-
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
-	const pageCount = Math.ceil(itemCount / pageSize);
+	const router = useRouter();
+	const searchParams = useSearchParams();
 
+	const pageCount = Math.ceil(itemCount / pageSize);
 	if (pageCount <= 1) return null;
+
+	const changePage = (page: number) => {
+		const params = new URLSearchParams(searchParams);
+		params.set('page', page.toString());
+		router.push('?' + params.toString());
+	};
 
 	return (
 		<Flex
@@ -24,6 +35,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
 				color="gray"
 				variant="soft"
 				disabled={currentPage === 1}
+				onClick={() => changePage(1)}
 			>
 				<DoubleArrowLeftIcon />
 			</Button>
@@ -31,6 +43,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
 				color="gray"
 				variant="soft"
 				disabled={currentPage === 1}
+				onClick={() => changePage(currentPage - 1)}
 			>
 				<ChevronLeftIcon />
 			</Button>
@@ -38,6 +51,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
 				color="gray"
 				variant="soft"
 				disabled={currentPage === pageCount}
+				onClick={() => changePage(currentPage + 1)}
 			>
 				<ChevronRightIcon />
 			</Button>
@@ -45,11 +59,11 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
 				color="gray"
 				variant="soft"
 				disabled={currentPage === pageCount}
+				onClick={() => changePage(pageCount)}
 			>
 				<DoubleArrowRightIcon />
 			</Button>
 		</Flex>
 	);
 };
-
 export default Pagination;
